23a58035b96aa4f9da70b62d7bf9a15f084b1c47
Sabina Sadowska <sadowwwska@gmail.com>
2020-03-11 23:13:08 +0100

    6 items added to Gdansk

---
+++ src/data.js
@@ -0,0 +1,108 @@
+/*  favorite: false,
+    priceRange: 'expensive', 'medium', 'cheap', 'free'
+    type: 'nature', 'sport', 'culture', 'restaurant' */
+
+const data = [
+    [
+        {
+            name: 'Park Oliwski',
+            favorite: false,
+            priceRange: 'free',
+            type: 'nature',
+            img: 'https://www.zatokapiekna.pl/slir/w730/images/uploads/articles/584/24-park_oliwski.jpg',
+            location:'Gdańsk',
+            descriptionShort: 'Zabytkowy park w gdańskiej dzielnicy Oliwa, nad Potokiem Oliwskim.',
+            descriptionLong: 'Początkiem dzisiejszego parku był przyklasztorny ogród założony przez cystersów. Prawdopodobnie był położony po wschodniej stronie starej siedziby opatów z XV wieku, dalej do Zatoki Gdańskiej rozciągał się nadmorski las, który rozcinał Potok Oliwski. Po wzniesieniu przez opata Franciszka Zaleskiego pod koniec pierwszej połowy XVII wieku nowej siedziby opatów, ogród został poszerzony w kierunku południowo-zachodnim, stykając się ze starszym ogrodem klasztornym. ajmłodszymi częściami parku są: położony na południowo-zachodnim krańcu fragment pomiędzy ul. Opata Jacka Rybińskiego a budynkiem Gdańskiego Seminarium Duchownego - dawny ogród użytkowy cysterskiego konwentu; oraz 2-hektarowy obszar dawnego folwarku Saltzmanna z XVIII-XIX wieku wokół dworu, przejętego w 2001 na biura przez firmę Doraco, obsadzony m.in. grabami pospolitymi i kasztanowcami czerwonymi Briotti, bukszpanem, lawendą wąskolistną i tawulcem pogiętym. Obszar ten udostępniono 22 maja 2015. Na obszarze parku znajduje się również Spichlerz Opacki (Oddział Etnografii Muzeum Narodowego w Gdańsku)',
+        },
+        {
+            name: 'Rezerwat Mewia Łacha',
+            favorite: false,
+            priceRange: 'free',
+            type: 'nature',
+            img: 'https://pomorskie.travel/image/journal/article?img_id=242660&t=1542202897474',
+            location: 'Gdańsk',
+            descriptionShort: 'Rezerwat Mewia Łacha na Wyspie Sobieszewskiej to naturalne siedlisko fok i ptaków chronione przez ornitologów z Grupy KULING.',
+            descriptionLong: 'Rezerwat chroni miejsca lęgowe różnych gatunków rybitw i siedliska żerowania i odpoczynku ptaków siewkowych. Od roku 2007 jest to ponownie jedyne w Polsce miejsce gnieżdżenia się rybitw czubatych - ok. 570 par (2009). Gniazdują tu również rybitwy białoczelne, rybitwy rzeczne, sieweczki obrożne, ostrygojady, w przeszłości na łachach (piaszczystych wyspach) gniazdowały także kolonie mew srebrzystych i śmieszek, w rezerwacie notowano również lęgi rybitw popielatych oraz jedyny w Polsce przypadek lęgu sieweczki morskiej.'
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+        {
+            name: '',
+            favorite: false,
+            priceRange: '',
+            type: '',
+            img: 'url',
+            location:'Gdańsk',
+            descriptionShort: '',
+            descriptionLong: ''
+        },
+    ]
+]
