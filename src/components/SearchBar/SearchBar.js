7af8b86b075d4e5f09ec3ecde315fc6bb9829529
SabinaSadowska <sadowwwska@gmail.com>
2020-03-13 16:50:08 +0100

    searchBar.js added

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function SearchBar() {
+    const [value, setValue] = useState('')
+
+    const 
+
+    return <div>
+        <input placeholder="Wpisz nazwę" ></input>
+    </div>
+}
+
+export default SearchBar
--- src/pages/PlaceList/PlacesList.js
+++ src/pages/PlaceList/PlacesList.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 
 class PlaceList extends Component {
+    
+
     render() {
         return <div></div>
     }
