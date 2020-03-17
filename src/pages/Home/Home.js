10763c9b4b7d556d6c6ffe38b95826f55c061799
Julia Lojek <julialojek@gmail.com>
2020-03-17 19:30:55 +0100

    bar chart

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import FavBarChart from '../../components/FavBarChart/FavBarChart';
 import './Home.css';
 
 class Home extends Component {
     render() {
         return( 
         <main className="dashboard">
-            
+            <div className="dashboard__chart">
+                <h3 className="dashboard__chartTitle">Najpopularniejsze atrakcje:</h3>
+                <FavBarChart />
+            </div>
+            <div className="dashboard__chart">
+                <h3 className="dashboard__chartTitle">Podział wg miast:</h3>
+                <FavBarChart />
+            </div>
         </main>
         )
     }
