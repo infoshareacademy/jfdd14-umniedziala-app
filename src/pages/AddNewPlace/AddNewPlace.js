a67778d50f3f3fb34bcc8e6339317d728a188660
zazulec <60104519+zazulec@users.noreply.github.com>
2020-03-19 17:01:10 +0100

    type checkbox change into dropdown

--- src/pages/AddNewPlace/AddNewPlace.js
+++ src/pages/AddNewPlace/AddNewPlace.js
@@ -5,61 +5,58 @@ import { Link } from 'react-router-dom';
 import data from '../../data'
 
 const options = [
-  { key: 'Gda', text: 'Gdańsk', value: 'gdańsk' },
-  { key: 'Sop', text: 'Sopot', value: 'sopot' },
-  { key: 'Gdy', text: 'Gdynia', value: 'gdynia' },
+  { key: 'Gda', text: 'Gdańsk', value: 'Gdańsk' },
+  { key: 'Sop', text: 'Sopot', value: 'Sopot' },
+  { key: 'Gdy', text: 'Gdynia', value: 'Gdynia' },
+]
+
+const type = [
+  { key: 'Res', text: 'Restauracja', value: 'Restauracja' },
+  { key: 'Nat', text: 'Natura', value: 'Natura' },
+  { key: 'Spo', text: 'Sport', value: 'Sport' },
+  { key: 'Kul', text: 'Kultura', value: 'Kultura' },
 ]
 const atractionData = data;
 console.log(atractionData);
-console.log(atractionData.length.value);
+console.log(atractionData.length);
 
 class FormAddPlace extends Component {
-
   state = {
     name: "",
     id: "",
     favorite: false,
     priceRange: "",
-    typeRestaurant: false,
-    typeNatura: false,
-    typeSport: false,
-    typeKultura: false,
+    type: "",
     img: "",
     location: "",
     descriptionShort: "",
     descriptionLong: "",
     terms: false,
   }
+
+  // increment Id
+  incrementId = () => {this.setState({ id: this.state.id + 1 })};
+
   //handle Change Function
   handleChangeAtractionName = (e, { value }) => this.setState({ name: value });
   handleChangePhoto = (e, { value }) => this.setState({ img: value });
-  handleChangeLocation = (e, { value }) => this.setState({ location: value });
-  handleChangeCheckboxGastro = (e, { checked }) => { this.setState({ typeRestaurant: checked }) };
-  handleChangeCheckboxNatura = (e, { checked }) => { this.setState({ typeNatura: checked }) };
-  handleChangeCheckboxSport = (e, { checked }) => { this.setState({ typeSport: checked }) };
-  handleChangeCheckboxKultura = (e, { checked }) => { this.setState({ typeKultura: checked }) };
+  handleChangeLocation = (e, { value }) => this.setState({ location: value }); // location
+  handleChangeType = (e, { value }) => this.setState({ type: value }); // type
   handleChange = (e, { value }) => this.setState({ priceRange: value });
   handleChangeTextArea = (e, { value }) => this.setState({ descriptionLong: value });
   handleChangeTerms = (e, { checked }) => this.setState({ terms: checked });
-  
+
   // add To Local Storage Function
   addToLocalStorageaAtractionName = () => (localStorage.setItem('AtractionName', this.state.name));
   addToLocalStoragePhoto = () => (localStorage.setItem('Photo', this.state.img));
   addToLocalStorageLocation = () => (localStorage.setItem('Location', this.state.location));
-  addToLocalStorageCheckboxGastro = () => (localStorage.setItem('typeRestaurant', this.state.typeRestaurant));
-  addToLocalStorageCheckboxNatura = () => (localStorage.setItem('typeNatura', this.state.typeNatura));
-  addToLocalStorageCheckboxSport = () => (localStorage.setItem('typeSport', this.state.typeSport));
-  addToLocalStorageCheckboxKultura = () => (localStorage.setItem('typeKultura', this.state.typeKultura));
+  addToLocalStorageType = () => (localStorage.setItem('type', this.state.type));
   addToLocalStorage = () => (localStorage.setItem('Price', this.state.priceRange));
   addToLocalStorageTextAreaValue = () => (localStorage.setItem('Description', this.state.descriptionLong));
   addToLocalStorageTerms = () => (localStorage.setItem('Terms', this.state.terms));
-  //addToLocalStorageIncrementId = () => (localStorage.setItem('id', this.incrementId));
-  //increment Id
-  //incrementId = () => {this.setState({ id: this.state.id + 1 })
+  addToLocalStorageIncrementId = () => (localStorage.setItem('id', this.state.id));
     
-//};
 
- 
   //check term value
   checkTermValue = () => {
     if (this.state.terms === true) {
@@ -72,26 +69,21 @@ class FormAddPlace extends Component {
   };
   // add to data
   addToData = () => {
-    atractionData.push(this.state)
-    return console.log(atractionData)
+    atractionData.push(this.state);
+    return (console.log(atractionData));
   };
   // Wraped Function set on Add Atraction button
   wrapedFunction = () => {
     this.addToLocalStorageaAtractionName();
     this.addToLocalStoragePhoto();
     this.addToLocalStorageLocation();
-    this.addToLocalStorageCheckboxGastro();
-    this.addToLocalStorageCheckboxNatura();
-    this.addToLocalStorageCheckboxSport();
-    this.addToLocalStorageCheckboxKultura();
     this.addToLocalStorage();
     this.addToLocalStorageTextAreaValue();
     this.addToLocalStorageTerms();
-    this.addToLocalStorageIncrementId();
     this.incrementId();
+    this.addToLocalStorageIncrementId();
     this.addToData();
   };
-
 
   render() {
     return (
@@ -109,12 +101,12 @@ class FormAddPlace extends Component {
               placeholder='Wybierz lokalizację'
             />
           </Form.Group>
-          <Form.Group >
-            <label>Kategoria:</label>
-            <Form.Checkbox inline label='Restauracja' name='typeRestaurant' checked={this.state.typeRestaurant} onChange={this.handleChangeCheckboxGastro}></Form.Checkbox>
-            <Form.Checkbox inline label='Natura' name='typeNatura' checked={this.state.typeNatura} onChange={this.handleChangeCheckboxNatura}></Form.Checkbox>
-            <Form.Checkbox inline label='Sport' name='typeSport' checked={this.state.typeSport} onChange={this.handleChangeCheckboxSport}></Form.Checkbox>
-            <Form.Checkbox inline label='Kultura' name='typeKultura' checked={this.state.typeKultura} onChange={this.handleChangeCheckboxKultura}></Form.Checkbox>
+          <Form.Group>
+            <Form.Select input={this.state.type} onChange={this.handleChangeLocation}
+              label='Kategoria'
+              options={type}
+              placeholder='Wybierz kategorię'
+            />
           </Form.Group>
           <Form.Group inline>
             <label>Cena:</label>
