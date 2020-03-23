38ae8b0584230052a701532329c27a8e4173a94d
Sabina Sadowska <sadowwwska@gmail.com>
2020-03-23 19:19:09 +0100

    search by name working fine

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { Input } from 'semantic-ui-react';
 import { Button } from 'semantic-ui-react';
 import '../SearchBar/SearchBar.css';
@@ -7,20 +7,26 @@ import SearchAdvanced from '../SearchAdvanced/SearchAdvanced';
 import {data} from '../../data.js'
 
 
-function SearchBar(arr) {
+function SearchBar() {
 
     const [inputValue, setInputValue] = useState('');
     const [categoryValue, setCategoryValue] = useState('');
     const [results, setResults] = useState([])
 
-    /* const renderPlaces = () => 
-    props.data.map((el) => {
-        const name = el.name === inputValue;
+  
+    const getResults = () => {
+        return setResults(data.filter((el) => el.name.toLowerCase().includes(inputValue.toLowerCase())
+    ))
+    }
 
-        return name ? <div key={el.id}>
-            <h3>{el.name}</h3>
-        </div> : null
-    }) */
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    useEffect(() => {
+        getResults();
+    }, [inputValue]
+    );
 
     const renderPlaces = (arr) => 
     arr.map((el) => {
@@ -30,33 +36,10 @@ function SearchBar(arr) {
         </div> 
     })
 
-    // const getResults = () => {
-    //     setResults(data.filter((el) => {
-    //        return el.name.toLowerCase().includes(inputValue.toLowerCase())
-           
-    // }))
-    // }
-
-    const getResults = () => {
-        return setResults(data.filter((el) => el.name.toLowerCase().includes(inputValue.toLowerCase()).filter(el => el)
-        
-    ))
-    }
-    
-console.log(results)
-
-
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-        console.log(results);
-        console.log(inputValue)
-    }
-    {getResults()}
-
     return ( 
         <div className="search-bar__wrapper">
             <div className="search-bar__default">
-                <Input className="search-bar__input" action='Wyszukaj' placeholder='Wpisz nazwę...'  onChange={handleChange}  />
+                <Input className="search-bar__input" placeholder='Wpisz nazwę atrakcji...' value={inputValue} onChange={handleChange}  />
                 <ToggleButton text={"Zmień kryteria wyszukiwania"} name={"angle down"} fn={() => console.log(2112)}/>
             </div>
             <SearchAdvanced />
