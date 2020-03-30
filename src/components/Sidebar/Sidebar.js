04873fb0f15e711989522410be3bd144aba010dd
Julia Lojek <julialojek@gmail.com>
2020-03-30 17:25:27 +0200

    menu closing on clicking its links, default page added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,30 +1,15 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import './Sidebar.css';
 
 class Sidebar extends Component {
-    static propTypes = {
-        home: PropTypes.string.isRequired,
-        allList: PropTypes.string.isRequired,
-        favourite: PropTypes.string.isRequired,
-        addNew: PropTypes.string.isRequired
-    };
-    static defaultProps = {
-        home: "Strona główna",
-        allList: "Wszystkie atrakcje",
-        favourite: "Moje ulubione",
-        addNew: "Dodaj własną atrakcję"
-    };
-    
     render() {
         return (
         <nav className={this.props.visibility}>
-            {this.visibility}
-            <Link to="/" className="sidebar__link">{this.props.home}</Link>
-            <Link to="/placelist" className="sidebar__link">{this.props.allList}</Link>
-            <Link to="/placelist" className="sidebar__link">{this.props.favourite}</Link>
-            <Link to="/addnewplace" className="sidebar__link">{this.props.addNew}</Link>
+            <Link to="/" className="sidebar__link" onClick={this.props.changeVisibility}>Strona główna</Link>
+            <Link to="/placelist" className="sidebar__link" onClick={this.props.changeVisibility}>Wszystkie atrakcje</Link>
+            <Link to="/placelist" className="sidebar__link" onClick={this.props.changeVisibility}>Moje ulubione</Link>
+            <Link to="/addnewplace" className="sidebar__link" onClick={this.props.changeVisibility}>Dodaj własną atrakcję</Link>
         </nav>
         )
     }
