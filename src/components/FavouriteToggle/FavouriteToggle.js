329f1bde6b949ea50d7eed671d12f21becee1039
Julia Lojek <julialojek@gmail.com>
2020-04-01 17:33:37 +0200

    tooltip added, source of truth corrected

--- src/components/FavouriteToggle/FavouriteToggle.js
+++ src/components/FavouriteToggle/FavouriteToggle.js
@@ -1,36 +1,39 @@
 import React, {Component} from 'react';
 import './FavouriteToggle.css';
-import atractionData from '../../atractionData';
+import attractionData from '../../attractionData';
 
 class FavouriteToggle extends Component {
   state = {
-    list: atractionData,
-    object: atractionData.find(el => el.id === this.props.id),
-    favStatus: false
+    attractionList: attractionData,
+    attractionFavouriteStatus: attractionData.find(el => el.id === this.props.id).favorite
   };
 
-  heartFull = <i className="fas fa-heart dashboardFavourite__icon"></i>;
-  heartEmpty = <i className="far fa-heart dashboardFavourite__icon"></i>;
+  heartFull = <i className="fas fa-heart dashboardFavourite__icon" />;
+  heartEmpty = <i className="far fa-heart dashboardFavourite__icon" />;
 
-  componentDidMount() {
-    this.setState({ favStatus: this.state.object.favorite });
-  };
-
-  toggleOnClick = () => {
-    this.setState({ favStatus: !this.state.favStatus });
-    const listModified = [...this.state.list];
-    listModified.forEach(el => {
-      if (el.id === this.props.id) {
-        el.favorite = !el.favorite;
-      };
-    });
-    localStorage.setItem("atractionData", JSON.stringify(listModified));
+  toggleClick = () => {
+    const listModified = this.state.attractionList.map(
+      el => 
+        el.id !== this.props.id ?
+          el 
+          :
+          { 
+            ...el, 
+            favorite: !el.favorite 
+          }
+    );
+    this.setState({ attractionList: listModified });
+    this.setState({ attractionFavouriteStatus: listModified.find(el => el.id === this.props.id).favorite });
+    localStorage.setItem("attractionData", JSON.stringify(listModified));
   }
 
   render() {
-    const heartIcon = this.state.favStatus ? this.heartFull : this.heartEmpty;
+    const heartIcon = this.state.attractionFavouriteStatus ? this.heartFull : this.heartEmpty;
+    const tooltipText = this.state.attractionFavouriteStatus ? "usuń z ulubionych" : "dodaj do ulubionych";
     return (
-      <span onClick={this.toggleOnClick}>{heartIcon}</span>
+      <span className="tooltip" onClick={this.toggleClick}>{heartIcon}
+        <span className="tooltiptext">{tooltipText}</span>
+      </span>
     )
   }
 }
