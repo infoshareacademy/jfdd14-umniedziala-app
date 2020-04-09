9a107eb14c5e7a722e48365799a48a9f6656a7be
Julia Lojek <julialojek@gmail.com>
2020-04-09 15:56:08 +0200

    services.js cleaned, readme modified

--- src/services.js
+++ src/services.js
@@ -1,28 +1,12 @@
 const ATTRACTIONS_URL = "https://tripcity-app.firebaseio.com/attractions/";
 
-// TODO this file should be remade once we move to firebase
-// import attractions from "./fixtures/attractions.json";
-
-// const attractionsKey = "attractions";
-// Initially just to have some attractions in localStorage we prefill it
-// with the data we have prepared for testing purpose.
-// if (localStorage.getItem(attractionsKey) === null) {
-//   localStorage.setItem(attractionsKey, JSON.stringify(attractions));
-// }
-
 const favoriteAttractionIdsKey = "favoriteAttractionIds";
 const getFavoriteAttractionIdsFromLocalStorage = () =>
   JSON.parse(localStorage.getItem(favoriteAttractionIdsKey));
 
-// The exported function bodies should be remade once we move to firebase
-// Every single one of them returns a promise because in the end we are
-// going to get the data asynchronously from server
-
-// GET ATTRACTIONS
 export const getAttractions = () =>
   fetch(ATTRACTIONS_URL + ".json").then((response) => response.json());
 
-// CONVERT ATTRACTIONS OBJECT TO ARRAY
 export const getAttractionsAsArray = () =>
   getAttractions().then((attractions) =>
     Object.entries(attractions || {})
@@ -39,7 +23,6 @@ export const getAttractionById = (id) =>
 export const getFavoriteAttractionIds = () =>
   Promise.resolve(getFavoriteAttractionIdsFromLocalStorage() || null);
 
-// ADD NEW ATTRACTION
 export const addAttraction = (attraction) =>
   fetch(ATTRACTIONS_URL + ".json", {
     method: "POST",
