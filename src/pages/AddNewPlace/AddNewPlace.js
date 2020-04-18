08835ebbd405de53b74c498cf8c78203b13fcace
zazulec <60104519+zazulec@users.noreply.github.com>
2020-04-19 01:35:12 +0200

    login and register tast ready

--- src/pages/AddNewPlace/AddNewPlace.js
+++ src/pages/AddNewPlace/AddNewPlace.js
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { css } from 'glamor';
 
 
+
 const options = [
   { key: 'Gda', text: 'Gdańsk', value: 'Gdańsk' },
   { key: 'Sop', text: 'Sopot', value: 'Sopot' },
