6b13e6b6c00d15bdab9c0b22c44567a54f744a71
Sabina Sadowska <sadowwwska@gmail.com>
2020-04-19 21:34:34 +0200

    live update ready

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import {BrowserRouter, Switch, Route} from'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import AddNewPlace from './pages/AddNewPlace/AddNewPlace';
-import PlaceDetails from './pages/PlaceDetails/PlaceDetails';
-import PlaceList from './pages/PlaceList/PlacesList';
-import FavouriteList from './pages/FavouriteList/FavouriteList';
-import Default from './pages/Default/Default';
-import 'semantic-ui-css/semantic.min.css';
-import { ToastContainer } from 'react-toastify';
-import { getAttractionsAsArray } from './services';
-import { AttractionsContext } from './contexts/AttractionsContext';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import AddNewPlace from "./pages/AddNewPlace/AddNewPlace";
+import PlaceDetails from "./pages/PlaceDetails/PlaceDetails";
+import PlaceList from "./pages/PlaceList/PlacesList";
+import FavouriteList from "./pages/FavouriteList/FavouriteList";
+import Default from "./pages/Default/Default";
+import "semantic-ui-css/semantic.min.css";
+import { ToastContainer } from "react-toastify";
+import { getAttractionsAsArray } from "./services";
+import { AttractionsContext } from "./contexts/AttractionsContext";
 
-function App () {
+function App() {
   const [attractionList, setAttractionList] = useState([]);
 
   const attractionData = {
     attractionList,
-    setAttractionList
+    setAttractionList,
   };
 
+  /* useEffect(() => {
+    getAttractionsAsArray().then((attractions) =>
+      setAttractionList(attractions)
+    );
+  }, []); */
+
   useEffect(() => {
-    getAttractionsAsArray().then(attractions => setAttractionList(attractions))
+    getAttractionsAsArray().then((attractions) =>
+      setAttractionList(attractions)
+    );
+    const interval = setInterval(() => {
+      getAttractionsAsArray().then((attractions) =>
+        setAttractionList(attractions)
+      );
+    }, 3000);
+    return () => clearInterval(interval);
   }, []);
-  
-  return(
+
+  return (
     <BrowserRouter>
       <AttractionsContext.Provider value={attractionData}>
         <Header />
         <ToastContainer />
-        <Switch>       
-            <Route exact path="/" component={Home} />
-            <Route path="/addnewplace" component={AddNewPlace} />
-            <Route path="/placedetails/:id" component={PlaceDetails} />
-            <Route path="/placelist" component={PlaceList} />
-            <Route path="/myfavourites" component={FavouriteList} />
-            <Route component={Default} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/addnewplace" component={AddNewPlace} />
+          <Route path="/placedetails/:id" component={PlaceDetails} />
+          <Route path="/placelist" component={PlaceList} />
+          <Route path="/myfavourites" component={FavouriteList} />
+          <Route component={Default} />
         </Switch>
       </AttractionsContext.Provider>
-      
     </BrowserRouter>
   );
-  
 }
 
 export default App;
