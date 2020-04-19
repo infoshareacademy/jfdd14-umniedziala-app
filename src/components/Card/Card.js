8bc4e4f20d5fab7769d6f8df2286a41f9676e781
Julia Lojek <julialojek@gmail.com>
2020-04-19 10:18:04 +0200

    card link repaired not to refresh the website

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
-import './Card.css'
-
-
+import React from "react";
+import { Card } from "semantic-ui-react";
+import "./Card.css";
+import { Link } from "react-router-dom";
 
 const CardComponent = (props) => (
-    <div className="cardComponent">
-    <Card
-        href={props.link}
+  <div className="cardComponent">
+    <Link to={props.link}>
+      <Card
         image={props.image}
         meta={props.meta}
         description={props.description}
         extra={props.price}
         className="cardView"
-    />
-    </div>
-)
+      />
+    </Link>
+  </div>
+);
 
 export default CardComponent;
 
