87e5dc9f3900780e571371c0cd0ab2517cd53118
Robert Pasieczny <pasrobert@icloud.com>
2020-04-19 17:04:38 +0200

    19.04

---
+++ src/pages/Login/LoginPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LoginPage.css";
+import { Button } from "semantic-ui-react";
+
+const LoginPage = () => {
+  return (
+    <div className="loginPage__dashboard">
+      <div className="loginPage__header">
+        <p>Zaloguj się, aby dodawać i zobaczyć swoje ulubione atrakcje</p>
+      </div>
+      <div className="loginPage__button">
+        <Button basic color="blue" content="Zaloguj się" size="massive" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
